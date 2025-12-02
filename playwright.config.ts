0f634ba0064616c1aae68eaa63cfba42d4f30280
Joshua Pendragon <pendragon.josh@gmail.com>
2025-12-02 16:56:31 -0600

    ci: optimize test configuration for single-threaded execution and faster timeouts

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,12 +5,15 @@ export default defineConfig({
 	fullyParallel: true,
 	forbidOnly: !!process.env.CI,
 	retries: process.env.CI ? 2 : 0,
-	workers: process.env.CI ? 1 : undefined,
+	workers: 1, // single threaded until state can be per-instance
 	timeout: 20 * 1000,
 	reporter: [["html", { open: "never" }], ["dot"]],
 	use: {
 		trace: "on",
-		actionTimeout: 500,
+		actionTimeout: 100,
+	},
+	expect:{
+		timeout: 500
 	},
 	projects: [
 		{
