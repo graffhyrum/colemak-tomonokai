9f54894992d35a43044f552a8667506914d75451
Joshua Pendragon <pendragon.josh@gmail.com>
2025-12-03 15:41:19 -0600

    test: update fixture for dynamic project support

--- tests/fixture.ts
+++ tests/fixture.ts
@@ -1,16 +1,9 @@
 import { test as base } from "playwright/test";
-import { createQWERTYTypingTutorPage } from "./POM/qwertyTutorPage.ts";
+import type { ProjectOptions } from "./utils/getProjects.ts";
 
-type QWERTYTypingTutorPage = ReturnType<typeof createQWERTYTypingTutorPage>;
-// noinspection JSUnusedGlobalSymbols : false positive
-export const test = base.extend<{
-	qwertyPage: QWERTYTypingTutorPage;
-}>({
-	qwertyPage: async ({ page }, use) => {
-		const qwertyPage = createQWERTYTypingTutorPage(page);
-		await qwertyPage.goto();
-		await use(qwertyPage);
-	},
+export const test = base.extend<ProjectOptions>({
+	shape: ["ortho", { option: true }],
+	layout: ["qwerty", { option: true }],
 });
 
 // Export test.step for use in tests
