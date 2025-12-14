14d0e32fe9044dec77a630719ae6e36c866cfb98
Joshua Pendragon <pendragon.josh@gmail.com>
2025-12-14 14:36:43 -0600

    feat(playwright): enhance tests with screenshot support
    Add test for validating screenshots with masked elements. Update Playwright configuration with improved trace collection and reporter setup. Introduce a `test` script in `package.json` for streamlined testing.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -22,14 +15,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html', { open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
   },
 
   /* Configure projects for major browsers */
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,9 +1,18 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('file:///C:/Users/graff/WebstormProjects/colemak-tomonokai/index.html');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Colemak/);
+test.beforeEach(async ({ page }) => {
+	await page.goto(
+		"file:///C:/Users/graff/WebstormProjects/colemak-tomonokai/index.html",
+	);
 });
 
+test("has title", async ({ page }) => {
+	// Expect a title "to contain" a substring.
+	await expect(page).toHaveTitle(/Colemak/);
+});
+
+test("screenshot", async ({ page }) => {
+	await expect(page).toHaveScreenshot({
+		mask: [page.locator("h2.prompt")],
+	});
+});
